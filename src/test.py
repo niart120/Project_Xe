bba116b5b3e3dfa2d76e3fe3dde0f070d67e52fe
niart120 <38847256+niart120@users.noreply.github.com>
2021-12-03 03:17:20 +0900

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,11 +7,13 @@ def main():
 
     s0, s1 = prng.get_state()
     expected = (s0<<64)|s1
+    print(f"expected:{hex(expected)}")
+    
     observed = [prng.next()%2 for _ in range(128)]
     s = calc.getS()
     state_lst = calc.gauss_jordan(s, observed)
     state = calc.list2bitvec(state_lst)
-    print(f"expected:{hex(expected)}")
+
     print(f"result:{hex(state)}")
 
 if __name__ == "__main__":
